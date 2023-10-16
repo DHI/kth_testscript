6c7ff468e19a80866937d17537e7bb7dbb425a46
kthage <32189603+kthage@users.noreply.github.com>
2023-10-16 14:59:03 +0200

    Update Test3.py

--- NewFolder/Test3.py
+++ NewFolder/Test3.py
@@ -1,1 +1,10 @@
-ssss
+def Test3():
+    """
+    <Script>
+    <Author>admin</Author>
+    <Description>Please enter script description here</Description>
+    </Script>
+    """
+    # write your code here
+    print("jjjj")
+    pass;
