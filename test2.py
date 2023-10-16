ce4d17210689d4030088379bd860e9791647f8f5
kthage <32189603+kthage@users.noreply.github.com>
2023-10-16 10:32:10 +0200

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,2 +1,8 @@
 def _Test2():
+    """
+    <Script>
+    <Author>admin</Author>
+    <Description>Please enter script description here</Description>
+    </Script>
+    """
     return "from _Test2 native"
