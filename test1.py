19ad31954f10b3a74b0edea5145761243192a529
kthage <32189603+kthage@users.noreply.github.com>
2023-10-16 07:55:18 +0200

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,13 @@
+import test2
+
+def Test1():
+    """
+    <Script>
+    <Author>admin</Author>
+    <Description>Please enter script description here</Description>
+    </Script>
+    """
+    # write your code here
+    x = test2._Test2()
+    print (x)
+    pass;
